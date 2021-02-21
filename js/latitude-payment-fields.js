dba7e731eafb7bd56031ff77f9b54775b79a652f
Lizette Rabuya <lizette.rabuya@latitudefinancial.com>
2021-02-22 10:08:36 +1100

    rename js constant name to follow php constant name for plugin id

--- js/latitude-payment-fields.js
+++ js/latitude-payment-fields.js
@@ -1,10 +1,10 @@
 jQuery( function($){
 
   function validate_pay_method() {
-      var LATITUDE_PAYMENT_METHOD = 'latitude-checkout'
-      var current_payment = $('input[id=payment_method_' + LATITUDE_PAYMENT_METHOD + ']:checked', '#payment.woocommerce-checkout-payment').val();    
+      var PAYMENT_PLUGIN_ID = 'latitude-checkout'
+      var current_payment = $('input[id=payment_method_' + PAYMENT_PLUGIN_ID + ']:checked', '#payment.woocommerce-checkout-payment').val();    
       console.log('validate_pay_method: current_payment: ' + current_payment ); 
-      if (current_payment == LATITUDE_PAYMENT_METHOD ) {  
+      if (current_payment == PAYMENT_PLUGIN_ID ) {  
         $('button#place_order').hide();
       } else {  
         $('button#place_order').show();

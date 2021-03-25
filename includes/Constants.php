498bc9180c6fa68b524ffe953769aca57c1278c4
Latitude Checkout <lizette.rabuya@latitudefinancial.com>
2021-03-25 11:09:15 +1100

    re-added files deleted when folder was renamed

---
+++ includes/Constants.php
@@ -0,0 +1,25 @@
+<?php
+
+class Constants
+{
+    // plugin information
+    const WC_LATITUDE_GATEWAY_PLATFORM = 'woocommerce';
+    const WC_LATITUDE_GATEWAY_ID = 'latitudecheckout';
+    const WC_LATITUDE_GATEWAY_VERSION = '0.0.1';
+    const WC_LATITUDE_GATEWAY_NAME = 'Latitude Interest Free';
+
+    const AU_ICON_URL = 'https://assets.latitudefinancial.com/merchant-services/latitude/icon/latitude-interest-free.svg';
+    const NZ_ICON_URL = 'https://assets.latitudefinancial.com/merchant-services/latitude/icon/gem-interest-free.svg';
+
+    const ALLOWED_CURRENCY = ['AUD', 'NZD'];
+
+    const CALLBACK_ROUTE = '/wp-json/latitude/v1/callback';
+    const CANCEL_ROUTE = 'checkout/cart';
+    const COMPLETE_ROUTE = '/wc-api/latitude_checkout';
+
+    const PAYMENT_FIELDS_URL_PROD = 'https://master.checkout.dev.merchant-services-np.lfscnp.com';
+    const PAYMENT_FIELDS_URL_TEST = 'https://master.checkout.dev.merchant-services-np.lfscnp.com';
+
+    const WIDGETS_URL_PROD = 'https://checkout.latitudefinancial.com'; // to confirm if this url is the same for both test and prod env
+    const WIDGETS_URL_TEST = 'https://checkout.latitudefinancial.com';
+}
---
+++ includes/Latitude_Purchase_Request.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Latitude Checkout Payment Request Handler Class
+ */
+
+use Constants as LatitudeConstants;
+
+class Latitude_Purchase_Request
+{
+    /**
+     * Protected variables.
+     *
+     * @var		WC_LatitudeCheckoutGateway	$gateway		A reference to the WooCommerce Latitude Checkout Payment Gateway.
+     */
+    protected $gateway;
+
+    /**
+     * Class constructor. Called when an object of this class is instantiated.
+     *
+     */
+    public function __construct()
+    {
+        $this->gateway = WC_LatitudeCheckoutGateway::get_instance();
+    }
+
+    /**
+     * Builds the purchase request payload
+     *
+     */
+    public function build_parameters($order_id)
+    {
+        $order = wc_get_order($order_id);
+
+        $order_lines = $this->build_order_lines($order);
+        $payment_request = [
+            'merchantId' => $this->gateway->get_merchant_id(),
+            'merchantName' => get_option('blogname'),
+            'isTest' => $this->gateway->get_test_mode(),
+            'merchantReference' => strval($order_id),
+            'amount' => floatval($order->get_total()),
+            'currency' => $order->get_currency(),
+            'promotionReference' => '',
+            'customer' => [
+                'firstName' => $order->get_billing_first_name(),
+                'lastName' => $order->get_billing_last_name(),
+                'phone' => $order->get_billing_phone(),
+                'email' => $order->get_billing_email(),
+            ],
+            'billingAddress' => [
+                'name' => $order->get_formatted_billing_full_name(),
+                'line1' => $order->get_billing_address_1(),
+                'line2' => $order->get_billing_address_2(),
+                'city' => $order->get_billing_city(),
+                'postcode' => $order->get_billing_postcode(),
+                'state' => $order->get_billing_state(),
+                'countryCode' => $order->get_billing_country(),
+                'phone' => $order->get_billing_phone(),
+            ],
+            'shippingAddress' => [
+                'name' => $order->get_formatted_shipping_full_name(),
+                'line1' => $order->get_shipping_address_1(),
+                'line2' => $order->get_shipping_address_2(),
+                'city' => $order->get_shipping_city(),
+                'postcode' => $order->get_shipping_postcode(),
+                'state' => $order->get_shipping_state(),
+                'countryCode' => $order->get_shipping_country(),
+                'phone' => $order->get_billing_phone(),
+            ],
+            'orderLines' => $order_lines,
+            'merchantUrls' => [
+                'cancel' => WC()->cart->get_checkout_url(),
+                'callback' => '',
+                'complete' => __(
+                    get_site_url() . LatitudeConstants::COMPLETE_ROUTE
+                ),
+            ],
+            'totalDiscountAmount' => floatval($order->get_total_discount()),
+            'totalShippingAmount' => floatval($order->get_shipping_total()),
+            'totalTaxAmount' => floatval($order->get_total_tax()),
+            'platformType' => LatitudeConstants::WC_LATITUDE_GATEWAY_PLATFORM,
+            'platformVersion' => WC()->version,
+            'pluginVersion' => LatitudeConstants::WC_LATITUDE_GATEWAY_VERSION,
+        ];
+        return $payment_request;
+    }
+
+    /**
+     * Builds the order lines for the purchase request payload
+     *
+     */
+    private function build_order_lines($order)
+    {
+        $order_lines = [];
+        foreach ($order->get_items() as $key => $item):
+            $product = $item->get_product();
+
+            $shipping_required = !empty($product->get_shipping_class())
+                ? false
+                : true;
+            $order_line = [
+                'name' => $item->get_name(),
+                'productUrl' => $product->get_permalink(),
+                'sku' => $product->get_sku(),
+                'quantity' => $item->get_quantity(),
+                'unitPrice' => floatval($product->get_price()),
+                'amount' => floatval($item->get_total()),
+                'tax' => floatval($item->get_total_tax()),
+                'requiresShipping' => $shipping_required,
+                'isGiftCard' => false, //TODO
+            ];
+            array_push($order_lines, $order_line);
+        endforeach;
+
+        return $order_lines;
+    }
+
+    /**
+     * Formats floating value
+     *
+     */
+    private function floatval($number)
+    {
+        return number_format((float) $number, 2, '.', '');
+    }
+}

7d8f6d951dd7c360e5a660c7ce7b9df8994869bb
Latitude Checkout <lizette.rabuya@latitudefinancial.com>
2021-04-15 14:17:21 +1000

    feat:[sfpg_2294] update plugin version for code refactoring

--- checkout-plugins-woocommerce.php
+++ checkout-plugins-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Enabling Latitude Interest Free Payment Gateway on a WooCommerce store.
  * Author: latitudefinancial
  * Author URI: https://www.latitudefinancial.com.au/
- * Version:0.0.69
+ * Version:0.0.70
  * Text Domain: checkout-plugins-woocommerce
  * WC tested up to: 5.6
  *
@@ -35,7 +35,7 @@ define('WP_DEBUG_LOG', false);
 define('WP_DEBUG_DISPLAY', false);
 
 define('WC_LATITUDE_GATEWAY__MINIMUM_WP_VERSION', '5.6');
-define('WC_LATITUDE_GATEWAY__PLUGIN_VERSION', '0.0.69'); 
+define('WC_LATITUDE_GATEWAY__PLUGIN_VERSION', '0.0.70'); 
 define('WC_LATITUDE_GATEWAY__PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 if (!class_exists('LatitudeCheckoutPlugin')) {

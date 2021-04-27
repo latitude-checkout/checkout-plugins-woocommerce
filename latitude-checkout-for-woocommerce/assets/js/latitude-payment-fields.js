c5bf22d745e1390d4899534cb058e6c439c40dd3
Latitude Checkout <lizette.rabuya@latitudefinancial.com>
2021-04-27 12:42:04 +1000

    fix: remove console.log

--- latitude-checkout-for-woocommerce/assets/js/latitude-payment-fields.js
+++ latitude-checkout-for-woocommerce/assets/js/latitude-payment-fields.js
@@ -3,7 +3,6 @@ jQuery(function ($) {
     $('form.checkout').on('change', 'input[name^="payment_method"]', function () {
       var PAYMENT_GATEWAY_ID = 'latitudecheckout';
       var current_payment = $('input[id=payment_method_' + PAYMENT_GATEWAY_ID + ']:checked', '#payment.woocommerce-checkout-payment').val();
-      console.log(current_payment);
       if (current_payment == PAYMENT_GATEWAY_ID) {
         $('button#place_order').text('Choose a plan');
       } else {

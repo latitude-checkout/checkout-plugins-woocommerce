029357b3b2ed8f97b5f0654fcc51f44957c00de4
Latitude Checkout <lizette.rabuya@latitudefinancial.com>
2021-05-11 14:39:43 +1000

    improve loading of payment fields content

--- latitude-checkout-for-woocommerce/classes/wc-latitudecheckout-gateway.php
+++ latitude-checkout-for-woocommerce/classes/wc-latitudecheckout-gateway.php
@@ -289,64 +289,18 @@ if (!class_exists('WC_LatitudeCheckout_Gateway')) {
            
             ?>   
             <div id="latitude-payment--main"> 
-            <div style="display: flex !important; justify-content: center !important">
-            <svg
-                version="1.1"
-                style="height: 50px !important"
-                id="L4"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 100 100"
-                enable-background="new 0 0 0 0"
-                xml:space="preserve"
-            >
-                <circle fill="#0046AA" stroke="none" cx="6" cy="50" r="6">
-                <animate
-                    attributeName="opacity"
-                    dur="2s"
-                    values="0;1;0"
-                    repeatCount="indefinite"
-                    begin="0.1"
-                />
-                </circle>
-                <circle fill="#0046AA" stroke="none" cx="26" cy="50" r="6">
-                <animate
-                    attributeName="opacity"
-                    dur="2s"
-                    values="0;1;0"
-                    repeatCount="indefinite"
-                    begin="0.2"
-                />
-                </circle>
-                <circle fill="#0046AA" stroke="none" cx="46" cy="50" r="6">
-                <animate
-                    attributeName="opacity"
-                    dur="2s"
-                    values="0;1;0"
-                    repeatCount="indefinite"
-                    begin="0.3"
-                />
-                </circle>
-            </svg>
+            <div style="display: flex !important;">
+                <p style="margin-top: 14px !important; margin-bottom: 14px !important">
+                <strong>Enjoy Now. Pay Later.</strong>
             </div>
-            <p style="margin-top: 14px !important; margin-bottom: 14px !important">
+            <p style="margin-top: 14px !important; margin-bottom: 14px !important; text-align:left">
+                Flexible Interest Free Plans to suit your needs
+            </p>
+            <p style="margin-top: 14px !important; margin-bottom: 14px !important; font-size: 12px; text-align:left">
                 You will be redirected to Latitude complete your order
             </p>
             </div>  
             <div id="latitude-payment--footer"></div> 
-            <script>
-                function reloadScript() {
-                        var curr = document.createElement("script");
-                        curr.type = "text/javascript";
-                        curr.async = true;
-                        curr.id = "latitude-payment-script";
-                        curr.src = '<?php echo $this->get_payment_fields_src(); ?>';
-                        var scr = document.getElementsByTagName("script")[0];
-                        scr.parentNode.insertBefore(curr, scr);
-                    }
-            </script>
             <?php
             
             wp_enqueue_script(
